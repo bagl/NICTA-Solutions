bee591c7d70d10a843320ed7664b0171e051e736
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-30 11:17:25 -0700

    Code golfing

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TupleSections #-}
 
 module Course.State where
 
@@ -26,23 +27,14 @@ import qualified Data.Set as S
 -- >>> instance Arbitrary a => Arbitrary (List a) where arbitrary = P.fmap listh arbitrary
 
 -- A `State` is a function from a state value `s` to (a produced value `a`, and a resulting state `s`).
-newtype State s a =
-  State {
-    runState ::
-      s
-      -> (a, s)
-  }
+newtype State s a = State { runState :: s -> (a, s) }
 
 -- | Implement the `Functor` instance for `State s`.
 -- >>> runState ((+1) <$> pure 0) 0
 -- (1,0)
 instance Functor (State s) where
-  (<$>) ::
-    (a -> b)
-    -> State s a
-    -> State s b
-  f <$> sa = State $ \s ->
-    first f $ runState sa s
+  (<$>) :: (a -> b) -> State s a -> State s b
+  f <$> sa = State $ first f . runState sa
 
 
 -- | Implement the `Apply` instance for `State s`.
@@ -53,31 +45,23 @@ instance Functor (State s) where
 -- >>> runState (State (\s -> ((+3), s P.++ ["apple"])) <*> State (\s -> (7, s P.++ ["banana"]))) []
 -- (10,["apple","banana"])
 instance Apply (State s) where
-  (<*>) ::
-    State s (a -> b)
-    -> State s a
-    -> State s b
+  (<*>) :: State s (a -> b) -> State s a -> State s b
   sf <*> sa = State $ \s ->
     let (f, sf') = runState sf s
-    in first f $ runState sa sf'
+    in runState (f <$> sa) sf'
 
 -- | Implement the `Applicative` instance for `State s`.
 -- >>> runState (pure 2) 0
 -- (2,0)
 instance Applicative (State s) where
-  pure ::
-    a
-    -> State s a
-  pure a = State $ \s -> (a, s)
+  pure :: a -> State s a
+  pure = State . (,)
 
 -- | Implement the `Bind` instance for `State s`.
 -- >>> runState ((const $ put 2) =<< put 1) 0
 -- ((),2)
 instance Bind (State s) where
-  (=<<) ::
-    (a -> State s b)
-    -> State s a
-    -> State s b
+  (=<<) :: (a -> State s b) -> State s a -> State s b
   f =<< sa = State $ \s ->
     let (a, sa') = runState sa s
     in runState (f a) sa'
@@ -87,37 +71,31 @@ instance Monad (State s) where
 -- | Run the `State` seeded with `s` and retrieve the resulting state.
 --
 -- prop> \(Fun _ f) -> exec (State f) s == snd (runState (State f) s)
-exec ::
-  State s a
-  -> s
-  -> s
-exec sa s = snd $ runState sa s
+exec :: State s a -> s -> s
+exec sa = snd . runState sa
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
 -- prop> \(Fun _ f) -> eval (State f) s == fst (runState (State f) s)
-eval ::
-  State s a
-  -> s
-  -> a
-eval sa s = fst $ runState sa s
+eval :: State s a -> s -> a
+eval sa = fst . runState sa
 
 -- | A `State` where the state also distributes into the produced value.
 --
 -- >>> runState get 0
 -- (0,0)
-get ::
-  State s s
-get = State $ \s -> (s, s)
+get :: State s s
+get = State $ join (,)
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
 -- >>> runState (put 1) 0
 -- ((),1)
-put ::
-  s
-  -> State s ()
-put s = State $ \_ -> ((), s)
+put :: s -> State s ()
+put = State . const ((),)
+
+getPut :: (s -> s) -> State s s
+getPut f = get >>= \s -> put (f s) >> pure s
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -134,11 +112,7 @@ put s = State $ \_ -> ((), s)
 -- >>> let p x = (\s -> (const $ pure (x == 'i')) =<< put (1+s)) =<< get in runState (findM p $ listh ['a'..'h']) 0
 -- (Empty,8)
 
-findM ::
-  Monad f =>
-  (a -> f Bool)
-  -> List a
-  -> f (Optional a)
+findM :: Monad f => (a -> f Bool) -> List a -> f (Optional a)
 findM _ Nil     = pure Empty
 findM p (a:.as) = p a >>= bool (findM p as) (pure $ Full a)
 
@@ -149,16 +123,10 @@ findM p (a:.as) = p a >>= bool (findM p as) (pure $ Full a)
 --
 -- prop> case firstRepeat xs of Empty -> let xs' = hlist xs in nub xs' == xs'; Full x -> length (filter (== x) xs) > 1
 -- prop> case firstRepeat xs of Empty -> True; Full x -> let (l, (rx :. rs)) = span (/= x) xs in let (l2, r2) = span (/= x) rs in let l3 = hlist (l ++ (rx :. Nil) ++ l2) in nub l3 == l3
-firstRepeat ::
-  Ord a =>
-  List a
-  -> Optional a
+firstRepeat :: Ord a => List a -> Optional a
 firstRepeat as = eval (findM p as) S.empty
   where
-    p a = do
-      sas <- get
-      put (S.insert a sas)
-      pure $ S.member a sas
+    p a = S.member a <$> getPut (S.insert a)
 
 -- | Remove all duplicate elements in a `List`.
 -- /Tip:/ Use `filtering` and `State` with a @Data.Set#Set@.
@@ -166,15 +134,9 @@ firstRepeat as = eval (findM p as) S.empty
 -- prop> firstRepeat (distinct xs) == Empty
 --
 -- prop> distinct xs == distinct (flatMap (\x -> x :. x :. Nil) xs)
-distinct ::
-  Ord a =>
-  List a
-  -> List a
+distinct :: Ord a => List a -> List a
 distinct as = eval (filtering p as) S.empty
-  where p a = do
-          sas <- get
-          put (S.insert a sas)
-          pure $ not (S.member a sas)
+  where p a = not . S.member a <$> getPut (S.insert a)
 
 -- | A happy number is a positive integer, where the sum of the square of its digits eventually reaches 1 after repetition.
 -- In contrast, a sad number (not a happy number) is where the sum of the square of its digits never reaches 1
