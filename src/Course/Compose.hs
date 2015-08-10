4b071974597bdf56419b316c3c1cbd0d9c345d21
Petr Vapenka <petr.vapenka@volny.cz>
2015-08-10 09:15:47 -0700

    Finish Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -11,29 +11,20 @@ import Course.Bind
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) f (Compose a) = Compose $ f <$$> a
 
-instance (Apply f, Apply g) =>
-  Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+instance (Apply f, Apply g) => Apply (Compose f g) where
+  (<*>) (Compose f) (Compose a) = Compose $ lift2 (<*>) f a
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure = Compose . pure . pure
 
-instance (Bind f, Bind g) =>
-  Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+instance (Bind f, Bind g) => Bind (Compose f g) where
+  (=<<) = error "impossible"
