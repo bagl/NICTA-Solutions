0232916a9772ff7453876afddb638546cb7c91e2
Petr Vapenka <vapenkap@obsd.krecci.dum>
2015-08-17 22:45:32 +0200

    Finish Anagrams and FastAnagrams

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -10,17 +10,15 @@ import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams :: Chars -> Filename -> IO (List Chars)
+fastAnagrams word fPath = validWords . lines <$> readFile fPath
+  where
+    isValidWord :: List Chars -> Chars -> Bool
+    isValidWord = flip S.member . S.fromList . hlist
+    validWords dict = filter (isValidWord dict) (permutations word)
 
-newtype NoCaseString =
-  NoCaseString {
-    ncString :: Chars
-  }
+newtype NoCaseString = NoCaseString
+                       { ncString :: Chars }
 
 instance Eq NoCaseString where
   (==) = (==) `on` map toLower . ncString
